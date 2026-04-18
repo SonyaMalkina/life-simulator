6fc1820c16588d4280bb9700c9f70d4f219e62f6
SonyaMalkina <sonamalkina5@gmail.com>
2026-04-18 17:00:28 +0300

    Исправлены main.py и ecosystem.py по PEP8

--- ecosystem.py
+++ ecosystem.py
@@ -1,15 +1,18 @@
 from organism import Organism
 
 class Ecosystem:
-   def __init__(self):
-      self.organisms = []
+    """ Класс симулирующий экосистему """
+    def __init__(self):
+        self.organisms = []
 
-   def add_organism(self, organism: Organism):
-      self.organisms.append(organism)
+    """ Добавление организма в экосистему """
+    def add_organism(self, organism: Organism):
+        self.organisms.append(organism)
 
-   def simulate_day(self):
-      for org in self.organisms:
-         if org.is_alive():
-            org.eat(10)
-         else:
-            print(f"{org.name} мёртв.")
+    """ Симуляция дня, проверка жив ли организм """ 
+    def simulate_day(self):
+        for org in self.organisms:
+            if org.is_alive():
+                org.eat(10)
+            else:
+                print(f"{org.name} мёртв.")
--- main.py
+++ main.py
@@ -2,14 +2,19 @@ from ecosystem import Ecosystem
 from organism import Organism
 
 def main():
-   eco = Ecosystem()
-   rabbit = Organism("Заяц", 20)
-   fox = Organism("Лиса", 30)
+    # Создание 3-х объектов
+    eco = Ecosystem()
+    rabbit = Organism("Заяц", 20)
+    fox = Organism("Лиса", 30)
+    plant = Organism("Растение", 30)
 
-   eco.add_organism(rabbit)
-   eco.add_organism(fox)
+    # Добавление
+    eco.add_organism(rabbit)
+    eco.add_organism(fox)
+    eco.add_organism(plant)
 
-   eco.simulate_day()
+    # Старт симуляции
+    eco.simulate_day()
 
 if __name__ == "__main__":
-   main()
+    main()
